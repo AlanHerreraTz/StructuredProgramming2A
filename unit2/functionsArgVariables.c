cf5e6ce24cf9d98a6fc96e669f2048df6ba360f3
AlanHerreraTz <herreratalan@gmail.com>
2021-06-23 16:31:03 -0500

    Functions Parameters Value and Adress

---
+++ unit2/functionsArgVariables.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "./utils.h"
+#include <stdlib.h>
+
+int var1 = 10;
+int var2 = 20;
+
+int main (int argc, char** argv){
+    printf( "Just main: var1= %d, var2= %d\n", var1, var2);
+    printf("Function was executed: \n");
+    //Paso de argumentos por direccion//
+    modifyVariables(var1, var2 );
+
+    printf( "var1: %d, var2: %d\n", var1, var2);
+
+    //Get the adress of var1 and var2
+    // % = "La direccion de" ---> &x0079fb
+
+    int * myptr1 = &var1;
+    int * myptr2 = &var2;
+
+    printf( "adress var1: %p, \t adress var2: %p\n", &var1, &var2 );
+    // *myptr1 = "El valor de" --> &x00879fb = x;
+    myptr1 = &var1;
+    *myptr1 = 20;
+    var2 = 40;
+
+    printf("var1 = %d, var2 = %d\n", var1 , var2);
+
+    showIntadress ( myptr1);
+    showIntadress ( myptr2 );
+
+    printf ("Pointer has modificated the var1: \n");
+
+    printf("///////////////////////////Pointers into functions///\n");
+
+    printf("var1= %d, var2 = %d\n", var1, var2);
+    printf("Function adress was executed:\n");
+    //Paso de argumentos por direccion//
+    modifyVariablesAdress( &var1, &var2, 3 );
+    //Decodificado humano: modifyVariablesAdress (&var1, &var2);
+    printf("var1= %d, var2= %d \n", var1, var2);
+    //Dentro del codigo debe ir algo asi:
+        // *0x7f2044a01018 = 100;
+
+    return 0;
+}
--- unit2/utils.h
+++ unit2/utils.h
@@ -20,3 +20,33 @@ int addtwonumbers (int argumento1, int argumento2){
     int result = argumento1 + argumento2 ;   
     return result; 
 }
+//Paso de argumentos por direccion //
+void modifyVariables (int argument1, int argument2 ){
+    argument1 = argument1*2;
+    argument2 = argument2*2;
+    return ;
+}
+
+//Paso de argumentos por direccion//
+
+
+void showIntadress ( int*  param1){
+    printf("adress: %p \n", param1 );
+
+}
+void showintvalueadress (int* param1){
+    printf("Value: %i \n", *param1);
+
+}
+
+void modifyVariablesAdress (int* dir1, int* dir2, int multiplo ){
+    showIntadress (dir1);
+    showintvalueadress (dir1);
+    showIntadress (dir2);
+    showintvalueadress(dir2);
+    *dir1 = *dir1 *multiplo;
+    *dir2 = *dir2 *multiplo;
+
+    return;
+//El unico tipo variable capaz de cachar una direccion es un pointer
+}
